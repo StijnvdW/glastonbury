87b2464059751dee3f896315f4bcd5ea431440f1
Stijn van der Wielen <stijnvanderwielen@gmail.com>
2016-03-02 22:11:56 +0100

    Updated view and template to show festivals. Css framework imported in template.

--- festivals/views.py
+++ festivals/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Festival
 
 # List of the festivals
 def festival_list(request):
-    return render(request, 'festivals/festival_list.html', {})
+    festivals = Festival.objects.all()
+    return render(request, 'festivals/festival_list.html', {'festivals': festivals})
