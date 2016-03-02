4ffd98d101d960f03de321aa748903fbbb824356
Stijn van der Wielen <stijnvanderwielen@gmail.com>
2016-03-02 21:00:55 +0100

    Adding app and festival model

---
+++ festivals/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Festival models
+class Festival(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+# Artist
+class Artist(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name= models.CharField(max_length=50)
+    record_label = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
