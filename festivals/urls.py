a3d97a8fa036047fcb18f4fcd20e386c5aee7475
Stijn van der Wielen <stijnvanderwielen@gmail.com>
2016-03-28 22:14:03 +0200

    Template structure and detail page

--- festivals/urls.py
+++ festivals/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from .import views
 
 urlpatterns = [
+    #index page: festivals/
     url(r'^$', views.festival_list, name='festival_list'),
+    #detail page: festivals/1/
+    url(r'^festival/(?P<festival_id>[0-9]+)/$', views.festival_detail, name='festival_detail'),
 ]
--- festivals/views.py
+++ festivals/views.py
@@ -5,3 +5,7 @@ from .models import Festival
 def festival_list(request):
     festivals = Festival.objects.all()
     return render(request, 'festivals/festival_list.html', {'festivals': festivals})
+
+def festival_detail(request, festival_id):
+    festival = Festival.objects.get(pk=festival_id)
+    return render(request, 'festivals/festival_detail.html', {'festival': festival})
