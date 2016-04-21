a441b4ad02720eedc1fdb2b2e99180e2525adf51
Stijn van der Wielen <stijnvanderwielen@gmail.com>
2016-04-21 20:31:43 +0200

    Datamodel rework Festival and Artist

--- festivals/admin.py
+++ festivals/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Artist
 from .models import Festival
+from .models import Performance
 
 
 admin.site.register(Artist)
 admin.site.register(Festival)
+admin.site.register(Performance)
--- festivals/models.py
+++ festivals/models.py
@@ -1,17 +1,5 @@
 from django.db import models
 
-# Festival models
-class Festival(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    start_date = models.DateField()
-    end_date = models.DateField()
-    logo = models.ImageField(upload_to='uploads/', default='/uploads/leeg.png')
-
-    def __str__(self):
-        return self.name
-
 # Artist
 class Artist(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -20,3 +8,28 @@ class Artist(models.Model):
 
     def __str__(self):
         return self.name
+
+# Festival models
+class Festival(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    logo = models.ImageField(upload_to='uploads/',
+    default='/uploads/leeg.png')
+    performers = models.ManyToManyField(Artist, through='Performance')
+
+    def __str__(self):
+        return self.name
+
+# Artist Performs on Fesitval
+class Performance(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    festival = models.ForeignKey(to='Festival', on_delete=models.CASCADE)
+    artist = models.ForeignKey(to='Artist', on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self):
+        return "{} performs on {} at {}".format(self.artist.name,
+        str(self.date), self.festival)
